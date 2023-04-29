a85f4ff570612e07cdf550a5aefd80eeb871fc97
unknown <deepakprakash990@outlook.com>
2023-04-29 22:15:52 +0530

    organizing files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App.js";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
